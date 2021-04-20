80730b3e98576fdf266d66ffce04835df84e7ee8
Alex Fuoco <alexfuoc@gmail.com>
2021-04-20 17:15:44 -0400

    Initial Commit

---
+++ Prj2_BeatMotion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prj2_BeatMotion
+//
+//  Created by Alex Fuoco on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
